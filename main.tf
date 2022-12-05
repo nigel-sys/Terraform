7a239e59211101eb46493250dff34b61ca2607cd
Nigel <pratyushnbaxla79@gmail.com>
2022-12-05 15:59:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,12 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_vpc" "AWS-instance" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
 resource "aws_security_group" "AWS-instance" {
   name        = "team15_ssh"
   description = "grant ssh permission"
@@ -52,7 +58,7 @@ resource "aws_instance" "AWS-instance" {
   ami = "ami-00daffb0d753df675"
   instance_type = "t2.micro"
   key_name = "team15_dependencies"
-  vpc_security_group_ids = [aws_security_group.AWS-instance.id]
+  vpc_security_group_ids = [vpc_id]
   tags = {
     Name = "Team15"
   }
