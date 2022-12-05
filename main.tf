6ce67339121be3b5a5e492ab6a3223230a3dc516
Nigel <pratyushnbaxla79@gmail.com>
2022-12-05 14:36:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,11 +34,7 @@ resource "aws_vpc" "AWS-instance" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-  tags {
-    Name = "AWS-instance"
-  }
 }
-
 
 resource "aws_security_group" "AWS-instance" {
   name        = "team15_ssh"
