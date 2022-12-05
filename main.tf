1530bd7b8919d2dc6f9fb1b031c36816bf00210b
Nigel <pratyushnbaxla79@gmail.com>
2022-12-05 16:23:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,16 +13,9 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_vpc" "AWS-instance" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-}
-
 resource "aws_security_group" "AWS-instance" {
   name        = "team15_ssh"
   description = "grant ssh permission"
-  vpc_id = aws_vpc.AWS-instance.id
 
     ingress {
     from_port   = 22
@@ -58,7 +51,7 @@ resource "aws_instance" "AWS-instance" {
   ami = "ami-00daffb0d753df675"
   instance_type = "t2.micro"
   key_name = "team15_dependencies"
-  vpc_security_group_ids = [aws_security_group.AWS-instance.vpc_id]
+  vpc_security_group_ids = [aws_security_group.AWS-instance.id]
   tags = {
     Name = "Team15"
   }
